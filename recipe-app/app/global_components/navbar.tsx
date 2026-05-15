4e7233011211ce271220bc8f66f1aafbc1b10dd3
Pat Messiter <>
2026-05-15 15:50:37 +1000

    Updated navbar with some basic styling and applied it to the layout

--- recipe-app/app/global_components/navbar.tsx
+++ recipe-app/app/global_components/navbar.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link"
+
 export default function NavBar() {
     return (
-        <nav className="w-full justify-between">
-            <Link href="/">Search</Link>
-            <Link href="/">Shopping List</Link>
-            <Link href="/">Surprise Me</Link>
+        <nav className="w-full h-20 flex justify-center">
+            <div className="w-3/4 h-full flex justify-between items-center">
+                <header className="w-1/2">
+                    <h2>Easy Meal</h2>
+                </header>
+                <div className="w-1/4 flex justify-between">
+                    <Link href="/">Search</Link>
+                    <Link href="/">Shopping List</Link>
+                    <Link href="/">Surprise Me</Link>
+                </div>
+            </div>
         </nav>
     );
 }
