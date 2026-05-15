52394f92777b164439015099e7404b106660eb99
Pat Messiter <>
2026-05-15 15:28:10 +1000

    Added placeholder navbar component and a recipes page

---
+++ recipe-app/app/global_components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+    return (
+        <nav className="w-full justify-between">
+            <Link href="/">Search</Link>
+            <Link href="/">Shopping List</Link>
+            <Link href="/">Surprise Me</Link>
+        </nav>
+    );
+}
