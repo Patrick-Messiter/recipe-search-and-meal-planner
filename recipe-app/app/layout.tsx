82705f001db183bf3ca59e0ffe018b6de5c6effc
Pat Messiter <>
2026-05-15 19:58:57 +1000

    refactored components directory

--- recipe-app/app/layout.tsx
+++ recipe-app/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "./global_components/navbar.tsx";
+import NavBar from "@/components/navbar.tsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
