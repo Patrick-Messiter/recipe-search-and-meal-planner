e0366508458f920af13dd8d615f42618bf81a662
Pat Messiter <>
2026-05-15 16:31:23 +1000

    Applied styling to navbar component

--- recipe-app/app/global_components/navbar.tsx
+++ recipe-app/app/global_components/navbar.tsx
@@ -2,15 +2,15 @@ import Link from "next/link"
 
 export default function NavBar() {
     return (
-        <nav className="w-full h-20 flex justify-center">
+        <nav className="w-full h-20 flex justify-center bg-primary-green-600">
             <div className="w-3/4 h-full flex justify-between items-center">
                 <header className="w-1/2">
-                    <h2>Easy Meal</h2>
+                    <h2 className="text-primary-green-400 text-4xl font-semibold">Easy Meal</h2>
                 </header>
                 <div className="w-1/4 flex justify-between">
-                    <Link href="/">Search</Link>
-                    <Link href="/">Shopping List</Link>
-                    <Link href="/">Surprise Me</Link>
+                    <Link className="text-primary-green-400 hover:text-primary-green-200 font-bold" href="/">Search</Link>
+                    <Link className="text-primary-green-400 hover:text-primary-green-200 font-bold" href="/">Shopping List</Link>
+                    <Link className="text-primary-green-400 hover:text-primary-green-200 font-bold" href="/">Surprise Me</Link>
                 </div>
             </div>
         </nav>
